65e685a8f81bb1b18f7db82eb151c71f7256061f
Chris booker <cryanb91@gmail.com>
2023-01-04 21:30:41 -0500

    Added imports, export, and the skeleton for several methods

---
+++ src/modules/storage.js
@@ -0,0 +1,34 @@
+import { Project } from "./project";
+import { ToDo } from "./todo";
+import { Task } from "./task";
+
+function Storage(){
+
+  function getTodoList(){
+
+  }
+
+  function saveTodoList(){
+
+  }
+
+  function addProject(){
+
+  }
+
+  function deleteProject(){
+
+  }
+
+  function addTask(){
+
+  }
+
+  function deleteTask(){
+
+  }
+  return {getTodoList, saveTodoList, addProject,
+          deleteProject, addTask, deleteTask}
+}
+
+export {Storage}
