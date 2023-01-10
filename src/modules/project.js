f7517c6ed3e901e04803badb804ee18ae8cec828
Chris booker <cryanb91@gmail.com>
2023-01-10 18:42:04 -0500

    Renamed projectMethods function & removed leading underscore from property names

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,63 +1,63 @@
 import {isToday, isThisWeek,} from 'date-fns'
 
 function Project(projectName){
-  let _name = projectName;
-  let _tasks = [];
-  return Object.assign(ProjectMethods(), {_name, _tasks});
+  let name = projectName;
+  let tasks = [];
+  return Object.assign(ProjectProto(), {name, tasks});
 }
 
-function ProjectMethods(){
+function ProjectProto(){
   function getName(){
-    return this._name;
+    return this.name;
   }
 
   function setName(newName){
-    this._name = newName;
+    this.name = newName;
   }
 
   function getTasks(){
-    return this._tasks;
+    return this.tasks;
   }
 
   function getTask(taskName){
-    return this._tasks.some(task => task.getName() === taskName);
+    return this.tasks.some(task => task.getName() === taskName);
   }
 
   function setTasks(newTasks){
-    this._tasks = newTasks;
+    this.tasks = newTasks;
   }
 
   function addTask(newTask){
-    if (this._tasks.find(task => task.getName() === newTask.getName())){
+    if (this.tasks.find(task => task.getName() === newTask.getName())){
       return -1;
     }
-    this._tasks.push(newTask);
+    this.tasks.push(newTask);
   }
 
   function deleteTask(taskName){
-    const indexOfTask = this._tasks.findIndex(object => {
+    const indexOfTask = this.tasks.findIndex(object => {
       return object.getName() === taskName;
     });
     
     if (indexOfTask !== -1){
-      this._tasks.splice(indexOfTask,1);
+      this.tasks.splice(indexOfTask,1);
     }
   }
 
   function getTodaysTasks(){
-    return this._tasks.filter(task => isToday(task.getDueDate()));
+    return this.tasks.filter(task => isToday(task.getDueDate()));
   }
 
   function clearAllTasks(){
-    this._tasks.length = 0;
+    this.tasks.length = 0;
   }
 
   function getWeeksTasks(){
-    return this._tasks.filter(task => isThisWeek(task.getDueDate()));
+    return this.tasks.filter(task => isThisWeek(task.getDueDate()));
   }
 
   return Object.create({getName, setName, getTasks, setTasks, getTask, 
     addTask, deleteTask, clearAllTasks, getWeeksTasks, getTodaysTasks});
 }
 
-export {Project, ProjectMethods}
+export {Project, ProjectProto}
