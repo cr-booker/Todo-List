e1b3f1bc85afa3f5febf55a9a1679db922133641
Chris booker <cryanb91@gmail.com>
2023-01-10 20:27:47 -0500

    Updated imports, minor formatting changes and filled in empty function bodies for Storage object methods

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,32 +1,53 @@
-import { Project } from "./project";
-import { ToDo } from "./todo";
-import { Task } from "./task";
+import { TaskProto } from "./task";
+import { ProjectProto } from "./project";
+import { TodoProto } from "./todo";
 
 function Storage(){
-
   function getTodoList(){
+    const todoList = Object.assign(TodoProto(), JSON.parse(localStorage.getItem("todoable-list")));
+    todoList.setProjects(
+      todoList.getProjects()
+      .map(project => Object.assign(ProjectProto(), project))
+      );
 
+    todoList
+      .getProjects()
+      .forEach(project => 
+        project.setTasks(
+          project.getTasks().map(task => Object.assign(TaskProto(), task))
+        )
+      );
+    return todoList;
   }
 
-  function saveTodoList(){
-
+  function saveTodoList(data){
+    localStorage.setItem("todoable-list", JSON.stringify(data));
   }
 
-  function addProject(){
-
+  function addProject(project){
+    const todoList = getTodoList();
+    todoList.addProject(project);
+    saveTodoList(todoList);
   }
 
-  function deleteProject(){
-
+  function deleteProject(projectName){
+    const todoList = getTodoList();
+    todoList.deleteProject(projectName)
+    saveTodoList(todoList);
   }
 
-  function addTask(){
-
+  function addTask(projectName, task){
+    const todoList = getTodoList();
+    todoList.getProject(projectName).addTask(task);
+    saveTodoList(todoList);
   }
 
-  function deleteTask(){
-
+  function deleteTask(projectName, task){
+    const todoList = getTodoList();
+    todoList.getProject(projectName).deleteTask(task);
+    saveTodoList(todoList);
   }
+  
   return {getTodoList, saveTodoList, addProject,
           deleteProject, addTask, deleteTask}
 }
