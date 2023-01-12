1d6a54119996266b383f6f4e9e74fd397694d429
Chris booker <cryanb91@gmail.com>
2023-01-12 16:57:54 -0500

    Refactored and removed all \methods

--- src/modules/task.js
+++ src/modules/task.js
@@ -3,44 +3,7 @@ function Task(taskName, taskPriority="", taskDueDate="No due Date", taskDescript
   let priority = taskPriority;
   let dueDate = taskDueDate;
   let description = taskDescription;
-  return Object.assign(TaskProto(), {name, priority, description, dueDate});
+  return {name, priority, description, dueDate};
 }
 
-function TaskProto(){
-  function getName(){
-    return this.name;
-  }
-
-  function setName(newName){
-     this.name = newName;
-  }
-
-  function getPriority(){
-    return this.priority;
-  }
-
-  function setPriority(newPriorityLevel){
-    this.priority = newPriorityLevel;
-  }
-
-  function getDueDate(){
-    return this.dueDate;
-  }
-
-  function setDueDate(newDate){
-    this.dueDate = newDate;
-  }
-
-  function getDescription(){
-    return this.description;
-  }
-
-  function setDescription(newDescription){
-    this.description = newDescription;
-  }
-
- return Object.create({getName, setName, getPriority, setPriority, 
-  getDueDate, setDueDate, getDescription, setDescription});
-}
-
-export {Task, TaskProto}
+export {Task}
