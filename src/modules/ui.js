e5a18e0d371d315cfd2fb162704373103ccd3afb
Chris booker <cryanb91@gmail.com>
2023-01-12 19:14:17 -0500

    Refactored to have all methods attached to UI function and added a delegator method to handle clicks on project container

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -3,22 +3,22 @@ import { Project } from "./project";
 import { Todo } from "./todo";
 import { Storage } from "./storage";
 
-function UI(){
-  function init(){
+function UI(){}
+  UI.init = function(){
     initProjectBtns()
   }
   
-  function showProjectModal(){
+  UI.showProjectModal = function(){
     const projectModal = document.querySelector(".project-modal");
     projectModal.classList.remove("closed");
   }
 
-  function closeProjectModal(){
+  UI.closeProjectModal = function(){
     const projectModal = document.getElementsByClassName("project-modal")[0];
     projectModal.classList.add("closed");
   }
 
-  function addProject(){
+  UI.addProject = function(){
     const projectNameInput = document.getElementById("project-name-input");
     const inputErrorMessage = document.querySelector(".project-input-error");
     const projectInputContent = projectNameInput.value.trim();
@@ -34,11 +34,11 @@ function UI(){
     inputErrorMessage.textContent = "";
     Storage.addProject(Project(projectInputContent));
     projectNameInput.value = "";
-    createProject(projectInputContent);
+    createProjectBtn(projectInputContent);
     closeProjectModal();
   }
 
-  function createProject(projectName){
+  UI.createProjectBtn = function(projectName){
     const userProjects = document.getElementsByClassName("user-projects-container")[0];
     const newProject =  document.createElement("button");
     newProject.classList.add("project", "user-project", "grid-container");
@@ -50,22 +50,49 @@ function UI(){
 
     const delIcon = document.createElement("span");
     delIcon.textContent = `\u00D7`;
-    delIcon.classList.add("delete-project-icon")
+    delIcon.classList.add("delete", "delete-project-icon");
     newProject.appendChild(projectNameContainer);
     newProject.appendChild(delIcon);
     userProjects.appendChild(newProject);
   }
 
-  function initProjectBtns(){
-    const showProjectModalBtn = document.querySelector(".new-project-btn");
-  showProjectModalBtn.addEventListener("click", showProjectModal);
-
-  const closeProjectModalBtn = document.querySelector(".project-cancel-btn");
-  closeProjectModalBtn.addEventListener("click", closeProjectModal);
-
-  const addProjectBtn = document.querySelector(".project-add-btn")
-  addProjectBtn.addEventListener("click", addProject);
+  UI.deleteProjectBtn = function(element){
+    const parentNode = element.parentNode;
+    const projectName = parentNode.querySelector('.user-project-name').textContent;
+    Storage.deleteProject(projectName);
+    const displayNext = parentNode.nextElementSibling || parentNode.previousElementSibling || document.getElementById("inbox");
+    displayProject(displayNext);
+    parentNode.remove();
   }
+
+  UI.displayProject = function(element){
+    const userProjectName = element.firstElementChild;
+    const projectHeading = document.getElementById("project-name");
+    projectHeading.textContent = userProjectName ? userProjectName.textContent : element.textContent;
+    // loadTasks(projectHeading.textContent);
+    }
   
 
-}
+  UI. delegateProjects = function(target){
+    if (target.classList.contains("delete")){
+      deleteProjectBtn(target);
+    }
+    else if (target.classList.contains("project")){
+      displayProject(target)
+    }
+  }
+
+  UI. initProjectBtns = function(){
+    const showProjectModalBtn = document.querySelector(".new-project-btn");
+    showProjectModalBtn.addEventListener("click", showProjectModal);
+
+    const closeProjectModalBtn = document.querySelector(".project-cancel-btn");
+    closeProjectModalBtn.addEventListener("click", closeProjectModal);
+
+    const addProjectBtn = document.querySelector(".project-add-btn");
+    addProjectBtn.addEventListener("click", addProject);
+
+  const projectsContainer = document.querySelector(".projects-container");
+  projectsContainer.addEventListener("click", e => delegateProjects(e.target));
+  }
+ 
